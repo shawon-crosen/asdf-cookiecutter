da9366ae0ee3105edf3561e6b7f133810e8bbf14
Shawon Crosen <shawon@circleci.com>
2021-07-20 16:45:49 -0400

    testing tags

--- lib/utils.bash
+++ lib/utils.bash
@@ -21,8 +21,8 @@ sort_versions() {
 
 list_github_tags() {
   git ls-remote --tags --refs "https://$GH_REPO" |
-    grep -o 'tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    grep -o 'refs/tags/.*' | cut -d/ -f3- #|
+    # sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
